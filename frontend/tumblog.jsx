616bf6660b3ccea7c3470790a1dffde113d555af
danna-aa <danna@berkeley.edu>
2019-11-04 16:41:08 -0800

    Setup

---
+++ frontend/tumblog.jsx
@@ -0,0 +1,8 @@
+// entry file
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root");
+    ReactDOM.render(<h1>Tumblog</h1>, root);
+});
