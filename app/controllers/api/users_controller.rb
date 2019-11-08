eb06f4535861ac355cdf0cdbbaab8c44f65e9839
danna-aa <danna@berkeley.edu>
2019-11-07 19:10:38 -0800

    make it better

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,8 +1,8 @@
 class Api::UsersController < ApplicationController
   
-  def new
-    @user = User.new
-  end
+  # def new
+  #   @user = User.new
+  # end
 
   def create
     @user = User.new(user_params)
