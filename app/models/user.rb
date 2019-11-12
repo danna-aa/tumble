fe5d99e4b57befa031912f2a72c027c43575bbc2
danna-aa <danna@berkeley.edu>
2019-11-11 16:14:42 -0800

    Fix bug for heroku push

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
     validates :username, :email, :password_digest, :session_token, presence: true, uniqueness: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
     validates :password, length: { minimum: 6 }, allow_nil: true
-    validates :private, inclusion: { in: [ true, false ] }
+    # validates :private, inclusion: { in: [ true, false ] }
     
     # user auth
     after_initialize :ensure_session_token
