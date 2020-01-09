284c66d66249d9dcf5a3700479e1ff9d9eef6334
danna-aa <danna@berkeley.edu>
2020-01-08 16:53:09 -0800

    RB

--- frontend/components/dashboard/create_post_forms/2_create_photo_post_form.jsx
+++ frontend/components/dashboard/create_post_forms/2_create_photo_post_form.jsx
@@ -1,186 +1,186 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createPost, createPhotoPost, createVideoPost, createAudioPost } from '../../../actions/post_actions';
-import { closeModal } from '../../../actions/modal_actions';
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import { createPost, createPhotoPost, createVideoPost, createAudioPost } from '../../../actions/post_actions';
+// import { closeModal } from '../../../actions/modal_actions';
 
 
-class CreateTextPostForm extends React.Component {
-    constructor(props) {
-        super(props);
+// class CreateTextPostForm extends React.Component {
+//     constructor(props) {
+//         super(props);
 
-        this.state = {
-            user_id: props.currentUser.id,
-            username: props.currentUser.username,
-            title: "",
-            body: "",
-            source: "",
-            source_alias: "",
-            link: "",
-            link_alias: "",
-            image_url: "",
-            video_url: "",
-            html: "",
-            root_post_id: "",
-            parent_post_id: "",
-            post_type: props.formType,
-            private: false,
-            attached_photos: null,
-            photo_file: null,
-            photo_url: null
-        };
+//         this.state = {
+//             user_id: props.currentUser.id,
+//             username: props.currentUser.username,
+//             title: "",
+//             body: "",
+//             source: "",
+//             source_alias: "",
+//             link: "",
+//             link_alias: "",
+//             image_url: "",
+//             video_url: "",
+//             html: "",
+//             root_post_id: "",
+//             parent_post_id: "",
+//             post_type: props.formType,
+//             private: false,
+//             attached_photos: null,
+//             photo_file: null,
+//             photo_url: null
+//         };
 
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+//         this.handleSubmit = this.handleSubmit.bind(this);
+//     }
 
-    update(field) {
-        return e => this.setState({
-            [field]: e.currentTarget.value
-        });
-    }
+//     update(field) {
+//         return e => this.setState({
+//             [field]: e.currentTarget.value
+//         });
+//     }
 
-    handleFile(e) {
-        e.preventDefault();
-        const file = e.currentTarget.files[0];
-        const fileReader = new FileReader();
-        fileReader.onloadend = () => {
-            this.setState({ photo_file: file, photo_url: fileReader.result });
-        };
+//     // handleFile(e) {
+//     //     e.preventDefault();
+//     //     const file = e.currentTarget.files[0];
+//     //     const fileReader = new FileReader();
+//     //     fileReader.onloadend = () => {
+//     //         this.setState({ photo_file: file, photo_url: fileReader.result });
+//     //     };
 
-        if (file) {
-            fileReader.readAsDataURL(file);
-        }
-    }
+//     //     if (file) {
+//     //         fileReader.readAsDataURL(file);
+//     //     }
+//     // }
 
-    handleSubmit(e) {
-        const post = Object.assign({}, this.state);
-        this.props.createPost(post);
-        this.props.closeModal();
-    }
+//     handleSubmit(e) {
+//         const post = Object.assign({}, this.state);
+//         this.props.createPost(post);
+//         this.props.closeModal();
+//     }
 
-    renderErrors() {
-        return (
-            <ul>
-                {this.props.errors.map((error, i) => (
-                    <li key={`error-${i}`}>
-                        {error}
-                    </li>
-                ))}
-            </ul>
-        );
-    }
+//     renderErrors() {
+//         return (
+//             <ul>
+//                 {this.props.errors.map((error, i) => (
+//                     <li key={`error-${i}`}>
+//                         {error}
+//                     </li>
+//                 ))}
+//             </ul>
+//         );
+//     }
 
-    render() {
-        const preview = this.state.photo_url ? <img src={this.state.photo_url} className="preview" /> : null;
+//     render() {
+//         const preview = this.state.photo_url ? <img src={this.state.photo_url} className="preview" /> : null;
 
-        return (
-            <div className="post-form-container">
+//         return (
+//             <div className="post-form-container">
 
-                <form className="post-form" onSubmit={this.handleSubmit}>
+//                 <form className="post-form" onSubmit={this.handleSubmit}>
 
-                    <div className="current-user">{this.props.currentUser.username} <i className="fas fa-chevron-down"></i></div>
+//                     <div className="current-user">{this.props.currentUser.username} <i className="fas fa-chevron-down"></i></div>
 
-                    <div>
-                        <input type="user_id"
-                            value={this.state.user_id}
-                            onChange={this.update('user_id')}
-                            className="post-form-input"
-                            placeholder={this.state.user_id}
-                            type="hidden"
-                        />
-                    </div>
+//                     <div>
+//                         <input type="user_id"
+//                             value={this.state.user_id}
+//                             onChange={this.update('user_id')}
+//                             className="post-form-input"
+//                             placeholder={this.state.user_id}
+//                             type="hidden"
+//                         />
+//                     </div>
 
-                    <div>
-                        <input type="username"
-                            value={this.state.username}
-                            onChange={this.update('username')}
-                            className="post-form-input"
-                            placeholder={this.state.username}
-                            type="hidden"
-                        />
+//                     <div>
+//                         <input type="username"
+//                             value={this.state.username}
+//                             onChange={this.update('username')}
+//                             className="post-form-input"
+//                             placeholder={this.state.username}
+//                             type="hidden"
+//                         />
 
-                    </div>
+//                     </div>
 
-                    <div>
-                        <input type="image_url"
-                            value={this.state.image_url}
-                            onChange={this.update('image_url')}
-                            className="post-form-input image_url"
-                        />
-                        <div className="caption">Add photo</div>
-                    </div>
+//                     <div>
+//                         <input type="image_url"
+//                             value={this.state.image_url}
+//                             onChange={this.update('image_url')}
+//                             className="post-form-input image_url"
+//                         />
+//                         <div className="caption">Add photo</div>
+//                     </div>
 
-                    <div>
-                        <input type="file" 
-                            onChange={this.handleFile.bind(this)} 
-                            multiple="multiple"/>
-                    </div>
+//                     {/* <div>
+//                         <input type="file" 
+//                             onChange={this.handleFile.bind(this)} 
+//                             multiple="multiple"/>
+//                     </div> */}
 
-                   {preview}
+//                    {preview}
 
-                    <div>
-                        <input type="source"
-                            value={this.state.source}
-                            onChange={this.update('source')}
-                            className="post-form-input post-source"
-                            placeholder="Source"
-                        />
-                    </div>
+//                     <div>
+//                         <input type="source"
+//                             value={this.state.source}
+//                             onChange={this.update('source')}
+//                             className="post-form-input post-source"
+//                             placeholder="Source"
+//                         />
+//                     </div>
 
-                    <div>
-                        <input type="source_alias"
-                            value={this.state.source_alias}
-                            onChange={this.update('source_alias')}
-                            className="post-form-input source_alias"
-                            placeholder="Source alias"
-                        />
-                    </div>
+//                     <div>
+//                         <input type="source_alias"
+//                             value={this.state.source_alias}
+//                             onChange={this.update('source_alias')}
+//                             className="post-form-input source_alias"
+//                             placeholder="Source alias"
+//                         />
+//                     </div>
 
-                    <div>
-                        <input type="tags"
-                            value={this.state.tags}
-                            className="post-form-input tags"
-                            placeholder="#tags"
-                        />
-                    </div>
+//                     <div>
+//                         <input type="tags"
+//                             value={this.state.tags}
+//                             className="post-form-input tags"
+//                             placeholder="#tags"
+//                         />
+//                     </div>
 
-                    <div className="modal-click">
+//                     <div className="modal-click">
 
-                        <button className="close-button"
-                            onClick={this.props.closeModal}>
-                            <div className="unselected">Close</div>
-                        </button>
+//                         <button className="close-button"
+//                             onClick={this.props.closeModal}>
+//                             <div className="unselected">Close</div>
+//                         </button>
 
-                        <button className="post-button" type="submit">
-                            <div className="unselected">Post</div>
-                        </button>
+//                         <button className="post-button" type="submit">
+//                             <div className="unselected">Post</div>
+//                         </button>
 
-                    </div>
+//                     </div>
 
 
 
-                </form>
+//                 </form>
 
-            </div>
-        )
-    }
+//             </div>
+//         )
+//     }
 
-}
+// }
 
-const mapStateToProps = state => ({
-    currentUser: state.entities.users[state.session.id],
-    errors: state.errors.session,
-    formType: 'photo'
-})
+// const mapStateToProps = state => ({
+//     currentUser: state.entities.users[state.session.id],
+//     errors: state.errors.session,
+//     formType: 'photo'
+// })
 
-const mapDispatchToProps = dispatch => ({
-    createPost: post => dispatch(createPost(post)),
-    closeModal: () => dispatch(closeModal()),
-    // createPhotoPost: post => dispatch(createPhotoPost(post)),
-    // createVideoPost: post => dispatch(createVideoPost(post)),
-    // createAudioPost: post => dispatch(createAudioPost(post))
-});
+// const mapDispatchToProps = dispatch => ({
+//     createPost: post => dispatch(createPost(post)),
+//     closeModal: () => dispatch(closeModal()),
+//     // createPhotoPost: post => dispatch(createPhotoPost(post)),
+//     // createVideoPost: post => dispatch(createVideoPost(post)),
+//     // createAudioPost: post => dispatch(createAudioPost(post))
+// });
 
-export default connect(mapStateToProps, mapDispatchToProps)(CreateTextPostForm);
+// export default connect(mapStateToProps, mapDispatchToProps)(CreateTextPostForm);
 
 
 
@@ -410,3 +410,161 @@ export default connect(mapStateToProps, mapDispatchToProps)(CreateTextPostForm);
 
 // export default connect(mapStateToProps, mapDispatchToProps)(CreateTextPostForm);
 
+import React from "react";
+import { connect } from "react-redux";
+import {
+  createPost,
+  createPhotoPost,
+  createVideoPost,
+  createAudioPost
+} from "../../../actions/post_actions";
+import { closeModal } from "../../../actions/modal_actions";
+
+class CreateTextPostForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user_id: props.currentUser.id,
+      username: props.currentUser.username,
+      title: "",
+      body: "",
+      source: "",
+      source_alias: "",
+      link: "",
+      link_alias: "",
+      image_url: "",
+      video_url: "",
+      html: "",
+      root_post_id: "",
+      parent_post_id: "",
+      post_type: props.formType,
+      private: false
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  update(field) {
+    return e =>
+      this.setState({
+        [field]: e.currentTarget.value
+      });
+  }
+
+  handleSubmit(e) {
+    const post = Object.assign({}, this.state);
+    this.props.createPost(post);
+    this.props.closeModal();
+  }
+
+  renderErrors() {
+    return (
+      <ul>
+        {this.props.errors.map((error, i) => (
+          <li key={`error-${i}`}>{error}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div className="post-form-container">
+        <form className="post-form" onSubmit={this.handleSubmit}>
+          <div className="current-user">
+            {this.props.currentUser.username}{" "}
+            <i className="fas fa-chevron-down"></i>
+          </div>
+
+          <div>
+            <input
+              type="user_id"
+              value={this.state.user_id}
+              onChange={this.update("user_id")}
+              className="post-form-input"
+              placeholder={this.state.user_id}
+              type="hidden"
+            />
+          </div>
+
+          <div>
+            <input
+              type="username"
+              value={this.state.username}
+              onChange={this.update("username")}
+              className="post-form-input"
+              placeholder={this.state.username}
+              type="hidden"
+            />
+          </div>
+
+          <div image-post-form>
+            <input
+              type="image_url"
+              value={this.state.image_url}
+              onChange={this.update("image_url")}
+              className="post-form-input image_url"
+            />
+            <div className="caption">Add photo</div>
+          </div>
+
+          <div>
+            <input
+              type="source"
+              value={this.state.source}
+              onChange={this.update("source")}
+              className="post-form-input post-source"
+              placeholder="Source"
+            />
+          </div>
+
+          <div>
+            <input
+              type="source_alias"
+              value={this.state.source_alias}
+              onChange={this.update("source_alias")}
+              className="post-form-input source_alias"
+              placeholder="Source alias"
+            />
+          </div>
+
+          <div>
+            <input
+              type="tags"
+              value={this.state.tags}
+              className="post-form-input tags"
+              placeholder="#tags"
+            />
+          </div>
+
+          <div className="modal-click">
+            <button className="close-button" onClick={this.props.closeModal}>
+              <div className="unselected">Close</div>
+            </button>
+
+            <button className="post-button" type="submit">
+              <div className="unselected">Post</div>
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  currentUser: state.entities.users[state.session.id],
+  errors: state.errors.session,
+  formType: "text"
+});
+
+const mapDispatchToProps = dispatch => ({
+  createPost: post => dispatch(createPost(post)),
+  closeModal: () => dispatch(closeModal())
+  // createPhotoPost: post => dispatch(createPhotoPost(post)),
+  // createVideoPost: post => dispatch(createVideoPost(post)),
+  // createAudioPost: post => dispatch(createAudioPost(post))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreateTextPostForm);
