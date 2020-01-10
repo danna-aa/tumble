9583d1a4fed605eeb6df7eb97b3a7e0c411a9daa
danna-aa <danna@berkeley.edu>
2020-01-09 23:49:29 -0800

    Notes list

--- frontend/components/dashboard/post_show.jsx
+++ frontend/components/dashboard/post_show.jsx
@@ -7,12 +7,24 @@ import UserSidebar from '../user_sidebar/user_sidebar';
 class PostShow extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { userId: null };
+        this.state = { 
+            userId: null,
+            post: "",
+            loaded: false,
+        };
     }
 
     componentDidMount() {
         window.scrollTo(0, 0);
-        this.props.fetchSinglePost(this.props.match.params.postId)
+        this.props
+          .fetchSinglePost(this.props.match.params.postId)
+          .then(action =>
+            this.setState({ 
+                post: action.post,
+                loaded: true
+            })
+          );
+
     }
 
     handleBackToTop(e) {
@@ -28,7 +40,6 @@ class PostShow extends React.Component {
 
         // dashboard sorted in order of newest at the top
         let postsList = Object.values(posts).sort((a, b) => (a.created_at > b.created_at) ? -1 : 1);
-        let numPosts = postsList.length;
 
         // map list of dashboard items 
         let dashList = postsList.map(post => (
@@ -49,6 +60,45 @@ class PostShow extends React.Component {
 
         let currentUser = users[session.id];
 
+        let notesListSorted = null
+        if (this.state.loaded) {
+            let likes = this.state.post.likes;
+            let comments = this.state.post.comments;
+            let notesList = {...likes, ...comments}
+            notesListSorted = Object.values(notesList).sort((a, b) =>
+              a.created_at < b.created_at ? -1 : 1
+            );
+        }
+
+        let notesItems;
+        if (notesListSorted) {
+            notesItems = notesListSorted.map((item, i) => (
+
+                <div className="dashboard-item" key={i}>
+                    <div className="avatar">
+                        <img className="avatar-image"></img>
+                    </div>
+                    <div className="dashboard-background">
+                        <div className="temp">
+                            user id: {item.user_id}
+                            <br/>
+                            time: {item.created_at}
+                            <br/>
+                            {item.body ? "comment " + item.body : "like"}
+                        </div>
+                    
+                    </div>
+                </div>
+            ))
+        }
+
+        // console.log(this.state)
+        
+
+
+
+        // let notesList = {}
+
         return (
             <div className="dash">
                 {/* <PostForm /> */}
@@ -56,6 +106,9 @@ class PostShow extends React.Component {
 
                     {/* <PostFormButtons users={users} session={session} /> */}
                     {dashList}
+                    {notesItems}
+
+                    {/* {notesList} */}
 
                     <div className="back-to-top icon" onClick={this.handleBackToTop}><i className="fas fa-angle-double-up"></i></div>
 
